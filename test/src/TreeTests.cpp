c466c6da1efeccd3a5fadae80367f9e73990e077
tomergaf <tomergafsou@gmail.com>
2020-11-08 14:12:11 +0200

    uploaded tests

--- test/src/TreeTests.cpp
+++ test/src/TreeTests.cpp
@@ -3,3 +3,29 @@
 //
 
 #include "../include/TreeTests.h"
+#include <string>
+#include <vector>
+#include "../include/TestMain.h"
+
+TreeTests::TreeTests(): RT_1(1), RT_2(2), RT_3(3) {}
+
+void TreeTests::T_copyCtor_single() {
+    bool isPass=true;
+    std::string testName = "T_copyCtor_single";
+    std::vector<std::string> errors;
+    RootTree a(RT_1);
+    int expected = a.getNode();
+    int result = RT_1.getNode();
+    if(expected!=result){
+        isPass=false;
+        errors.push_back("Expected node is:" +std::to_string(expected) + "received node:" + std::to_string(result));
+    }
+    std::vector<std::string> childrenDiff = childrenEqual(&RT_1, &a);
+    if (childrenDiff.size()!= 0 )
+        isPass= false;
+    for (int i = 0; i < childrenDiff.size(); ++i) {
+        errors.push_back(childrenDiff[i]);
+    }
+    TestMain::assert(isPass, testName, errors);
+
+}
