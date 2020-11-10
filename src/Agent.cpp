f5840ef5b01913482c9b346618e7adcaeabc36a6
Orpazc <orpaz129@gmail.com>
2020-11-10 16:40:32 +0200

    change methods sign, add getters and setters

--- src/Agent.cpp
+++ src/Agent.cpp
@@ -1,10 +1,29 @@
 #include "../include/Agent.h"
 #include "../include/Session.h"
-Virus::Virus(int nodeInd): nodeInd(nodeInd) {}
-ContactTracer::ContactTracer() {}
+
+// Ctor
 Agent::Agent() {}
+
+// Contact Tracer
+
+ContactTracer::ContactTracer() {}
+
+void ContactTracer::act(Session &session) {}
+
+Agent * ContactTracer::clone() const {
+    return new ContactTracer;
+}
+
+// Virus
+
+Virus::Virus(int nodeInd): nodeInd(nodeInd) {}
+
 void Virus::act(Session &session) {}
 
-void ContactTracer::act(Session &session) {
+int Virus::getNodeInd() const {
+    return nodeInd;
+}
 
+Agent * Virus::clone() const {
+    return new Virus(nodeInd);
 }
--- src/Graph.cpp
+++ src/Graph.cpp
@@ -1,7 +1,22 @@
 #include "../include/Graph.h"
 
-Graph::Graph(std::vector<std::vector<int>> matrix){};
+#include <utility>
+using namespace std;
+
 Graph::Graph() {}
+
+Graph::Graph(vector<vector<int>> matrix){
+    edges = std::move(matrix);
+}
+
+Graph::Graph(const Graph &graph) {
+    vector<vector<int>> newEdges(getEdges());
+    edges = newEdges;
+}
+
+Graph * Graph::clone() const {
+    return new Graph(*this);
+}
 
 const std::vector<std::vector<int>> &Graph::getEdges() const {
     return edges;
