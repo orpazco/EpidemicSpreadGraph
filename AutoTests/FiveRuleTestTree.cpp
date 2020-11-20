94b1ce0d2888a75f8a20d5d138a5421732e1ce2d
tomergaf <tomergafsou@gmail.com>
2020-11-20 16:50:51 +0200

    fixed autotest

--- AutoTests/FiveRuleTestTree.cpp
+++ AutoTests/FiveRuleTestTree.cpp
@@ -15,14 +15,14 @@ void FiveRuleTestTree() {
     std::cout << "------Copy Test------" << std::endl;
     T test3 = *test1; //Tree Copy Constructor Tester, It shall choose the utilize the copy constructor since it is not initialized!
     delete(test1);
-    if(test3.getChildren()[0]->getRootLabel() == 3){
+    if(test3.getChildren()[0]->getNode() == 3){
         std::cout << "Passed | Copy Constructor Test" << std::endl;
     }else
         std::cout << "Failed | Copy Constructor Test" << std::endl;
     std::cout << "\r\n------Assignment Test------" << std::endl;
     T      testAssignment(5);
     testAssignment = (test3); //Assignment Constructor
-    if(testAssignment.getChildren()[0]->getRootLabel()  == 3)
+    if(testAssignment.getChildren()[0]->getNode()  == 3)
         std::cout << "Passed | Assignment Constructor Test " << std::endl;
     else
         std::cout << "Failed | Assignment Constructor Test " << std::endl;
@@ -34,25 +34,25 @@ void FiveRuleTestTree() {
     testMove1.addChild(*test2);
     delete(test2);
     testMove2 = std::move(testMove1);
-    if(testMove2.getChildren()[0]->getRootLabel() == 5 && testMove1.getChildren().size() == 0)
+    if(testMove2.getChildren()[0]->getNode() == 5 && testMove1.getChildren().size() == 0)
         std::cout << "Passed | Tree Assignment Test "<< std::endl;
     else
         std::cout << "Failed | Tree Assignment Test " << std::endl;
     testMove2 = std::move(testMove2); // Sanity check -> shouldn't trigger
-    if(testMove2.getChildren()[0]->getRootLabel() == 5 )
+    if(testMove2.getChildren()[0]->getNode() == 5 )
         std::cout << "Passed | Tree Assignment Test (Sanity)" << std::endl;
     else
         std::cout << "Failed | Tree Assignment Test (Sanity) " <<std::endl;
     T testMove3(std::move(T(1337)));
-    if(testMove3.getRootLabel() == 1337)
+    if(testMove3.getNode() == 1337)
         std::cout << "Passed | Tree Assignment Constructor Test" << std::endl;
     else
         std::cout << "Failed | Tree Assignment Constructor Test" << std::endl;
     std::cout << "-----------------------\r\n\r\n\n" << std::endl;
 }
-//int main(int argc,char **argv){
-//    FiveRuleTestTree<MaxRankTree>();
-//    FiveRuleTestTree<RootTree>();
-//}
+int main(int argc,char **argv){
+    FiveRuleTestTree<MaxRankTree>();
+    FiveRuleTestTree<RootTree>();
+}
 
 using namespace std;
